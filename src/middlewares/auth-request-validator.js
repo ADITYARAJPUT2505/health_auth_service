881ec41e50ae271643344877656458007da53f40
Aditya <ar9412349716@gmail.com>
2024-01-06 22:13:25 +0530

    added email and password validation for incoming request

---
+++ src/middlewares/auth-request-validator.js
@@ -0,0 +1,15 @@
+const validateUserSignin = (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({
+      success: false,
+      data: {},
+      message: "sommething went wrong ",
+      err: "email or password missing",
+    });
+  }
+  next();
+};
+
+module.exports = {
+  validateUserSignin,
+};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../../controller/user_controller");
 
-router.post("/signup", UserController.create);
-router.post("/signin", UserController.signIn);
+const { AuthRequestValidator } = require("../../middlewares/index");
+
+router.post("/signup", AuthRequestValidator.validateUserSignin);
+router.post("/signin", AuthRequestValidator.validateUserSignin);
 module.exports = router;
